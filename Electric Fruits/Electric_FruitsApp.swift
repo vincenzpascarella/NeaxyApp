29a287974a362997383f673d12cc5d5e3ace9889
vincenzpascarella <vincenz.pascarella@gmail.com>
2022-02-11 09:18:29 +0100

    Initial Commit

---
+++ Electric Fruits/Electric_FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Electric_FruitsApp.swift
+//  Electric Fruits
+//
+//  Created by Vincenzo Pascarella on 11/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Electric_FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
